504a183727647250be2b316c69f0266bc6294965
Ambuj Singh <57827514+ambuj2000@users.noreply.github.com>
2020-09-19 00:00:05 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,16 @@
+
+// Just a basics of javascript!!
+
+const x = 2;
+console.log(x);
+
+function demo(){
+    console.log("In function demo now");
+}
+demo();
+if(false){
+    console.log("In the the log");
+}
+else{
+    console.log("out of log");
+}
